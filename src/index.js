58dfeb6c84e686a679535e5beef04ca946170043
mohit <nvnkr1999@gmail.com>
2022-01-13 01:03:52 +0530

    Persist List Reordering with react-beautiful-dnd using the onDragEnd Callback

--- src/index.js
+++ src/index.js
@@ -6,8 +6,39 @@ import { DragDropContext } from 'react-beautiful-dnd';
 class App extends Component{
   state =initialData;
 
-  onDragEnd=result=>{
-    //code for end
+  onDragEnd = result => {
+    console.log("" ,result);
+    const { destination, source, draggableId } = result;
+ 
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+     ) {
+      return;
+     }
+     const column=this.state.columns[source.droppableId]
+     const newTaskIds=Array.from(column.taskIds);
+     newTaskIds.splice(source.index,1);
+     newTaskIds.splice(destination.index,0,draggableId)
+
+     const newColumn={
+       ...column,
+       taskIds:newTaskIds
+     }
+     const newState={
+
+       ...this.state,
+       columns:{
+         ...this.state.columns,
+         [newColumn.id]:newColumn
+       }
+
+     }
+     this.setState(newState);
+    
   }
   render(){
     return (
